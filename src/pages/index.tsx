1f41731063679f09f75e7e84c2c1d7334a153ffa
moshmatohead <xaviergabriel856@gmail.com>
2022-08-10 18:08:34 -0300

    rightside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,16 +16,30 @@ const Home: NextPage = () => {
   // const name: string = "Xavier";
   return (
     <div className="div__first_page">
-      <div className="div__left">
-        <img
-          src="https://www.alanaraujo.eng.br/static/media/image002.5f3f8cb5.png"
-          alt=""
-        />
-        <h1>
-          Faça seu login <br /> na plataforma
-        </h1>
+      <div className="container">
+        <div className="contend">
+          <div className="div__left">
+            <img
+              src="https://www.alanaraujo.eng.br/static/media/image002.5f3f8cb5.png"
+              alt=""
+            />
+            <h1>
+              Faça seu login <br /> na plataforma
+            </h1>
+          </div>
+          <div className="div__right">
+            <form action="#">
+              <input type="email" placeholder="E-mail" />
+              <input type="password" placeholder="Senha" />
+              <a href="#">Esqueci minha senha</a>
+              <button>Entrar</button>
+              <p>
+                Não tem uma conta? <a href="#">Registre-se</a>{" "}
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className="div__right">Parte 2</div>
     </div>
   );
 };
