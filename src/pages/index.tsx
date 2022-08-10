282cafd4c8378dbf63b414abb6b237ed7c36e949
moshmatohead <xaviergabriel856@gmail.com>
2022-08-10 16:37:44 -0300

    add_jsfunction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [name, setName] = useState<string>();
+  function changeName() {
+    setName("Xavier");
+  }
+  function clearName() {
+    setName("");
+  }
+
+  // const name: string = "Xavier";
   return (
     <div className="div__first_page">
-      <h1>Hello World!</h1>
-      <button className="button__change_color_white">Mudar de cor</button>
+      <h1>Hello World! {name} </h1>
+      <button
+        onBlur={clearName}
+        onClick={changeName}
+        className="button__change_color_white"
+      >
+        Mudar de cor
+      </button>
     </div>
   );
 };
